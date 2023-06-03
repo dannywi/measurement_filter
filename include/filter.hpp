d74aabc0350318ddb495269222b449a561d62be3
Danny W <54228968+dannywi@users.noreply.github.com>
2023-06-03 14:03:39 +0900

    Use common base class to assert signatures

--- include/filter.hpp
+++ include/filter.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 namespace mf {
+
 template <typename T>
-class filter_avg {
-  T latest_{0};
+class filter_base {
+ protected:
+  T latest_{};
+
+ public:
+  virtual void update(T) = 0;
+  virtual T current_estimate() const = 0;
+};
+
+template <typename T>
+class filter_avg : public filter_base<T> {
   unsigned int count_{0};
 
  public:
-  void update(T new_measurement) {
+  void update(T new_measurement) override {
     T alpha = 1.0 / static_cast<T>(++count_);
-    latest_ = alpha * new_measurement + (1.0 - alpha) * latest_;
+    this->latest_ = alpha * new_measurement + (1.0 - alpha) * this->latest_;
   }
 
-  T current_estimate() const { return latest_; }
+  T current_estimate() const override { return this->latest_; }
 };
 }  // namespace mf
