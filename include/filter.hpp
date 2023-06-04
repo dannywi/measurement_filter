3dbfb29e188c9befef844ece7935a4548600e513
Danny W <54228968+dannywi@users.noreply.github.com>
2023-06-04 13:04:52 +0900

    Add alpha beta filter

--- include/filter.hpp
+++ include/filter.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+
 namespace mf {
 
 template <typename T>
 class filter_base {
  protected:
-  T latest_{};
+  T current_estimate_{};
 
  public:
   virtual void update(T) = 0;
@@ -19,9 +23,51 @@ class filter_avg : public filter_base<T> {
  public:
   void update(T new_measurement) override {
     T alpha = 1.0 / static_cast<T>(++count_);
-    this->latest_ = alpha * new_measurement + (1.0 - alpha) * this->latest_;
+    this->current_estimate_ = alpha * new_measurement + (1.0 - alpha) * this->current_estimate_;
   }
 
-  T current_estimate() const override { return this->latest_; }
+  T current_estimate() const override { return this->current_estimate_; }
 };
+
+// todo: adapt to filter_base template
+template <typename T, typename TM = T>
+class filter_alpha_beta {
+  // high values for high precision (but prone to fluctuations)
+  // low values for low precision (but slow to adapt to changes)
+  float alpha_{1};  // weight of new observation
+  float beta_{1};   // weight of new observed velocity
+
+  T current_estimate_;  // todo: rename, should include "value"
+  T velocity_;          // todo: rename, this is also current estimate
+
+  TM last_time_{0};  // todo: abstract the concept, denominator of velocity
+
+ public:
+  filter_alpha_beta(float alpha, float beta) : alpha_(alpha), beta_(beta) {
+    if (alpha_ < 0 || beta_ < 0) throw std::runtime_error("invalid alpha beta");
+  }
+
+  void init(T value, T velocity) {
+    current_estimate_ = value;
+    velocity_ = velocity;
+  }
+
+  void update(T new_measurement, TM time) {
+    if (time < last_time_) { return; }
+    T estimate_from_prev = current_estimate(time);
+    T estimate_error = new_measurement - estimate_from_prev;
+
+    current_estimate_ = estimate_from_prev + alpha_ * estimate_error;
+    velocity_ += beta_ * estimate_error / (time - last_time_);
+
+    last_time_ = time;
+  }
+
+  T current_estimate(T time) const {
+    // might not be necessary to check in theory, but TM can be unsigned.
+    if (time < last_time_) { return current_estimate_; }
+    return current_estimate_ + velocity_ * std::max(0.0, time - last_time_);
+  }
+};
+
 }  // namespace mf
--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,7 @@
 
 #include "filter.hpp"
 
-constexpr double EPS = 1e-2;
+constexpr double EPS = 0.05;
 
 TEST(FILTER, Avg) {
   mf::filter_avg<double> f;
@@ -41,5 +41,53 @@ TEST(FILTER, Avg2) {
   for (auto t : tests) {
     f.update(t.new_val);
     EXPECT_NEAR(f.current_estimate(), t.estimate_exp, EPS);
+  }
+}
+
+TEST(FILTER, AlphaBeta1) {
+  mf::filter_alpha_beta<double> f{0.2, 0.1};
+  f.init(30000, 40);
+  EXPECT_NEAR(f.current_estimate(0), 30000, EPS);
+  EXPECT_NEAR(f.current_estimate(1), 30040, EPS);
+  EXPECT_NEAR(f.current_estimate(5), 30200, EPS);
+
+  f.update(30200, 5);
+  EXPECT_NEAR(f.current_estimate(5), 30200, EPS);
+
+  f.update(30300, 10);
+  EXPECT_NEAR(f.current_estimate(10), 30380, EPS);
+}
+
+TEST(FILTER, AlphaBeta2) {
+  mf::filter_alpha_beta<double, uint8_t> f{0.2, 0.1};
+
+  constexpr uint8_t INTERVAL = 5;
+
+  struct test {
+    double new_val;
+    double estimate_exp;
+  };
+
+  // clang-format off
+  std::vector<test> tests = {
+      {30171, 30194.2},
+      {30353, 30383.64},
+      {30756, 30612.73},
+      {30799, 30818.93},
+      {31018, 31025.7},
+      {31278, 31242.3},
+      {31276, 31418.8},
+      {31379, 31566.3},
+      {31748, 31739.4},
+      {32175, 31964.1},
+  };
+  // clang-format on
+
+  f.init(30000, 40);
+  uint8_t tm = 0;
+  for (auto t : tests) {
+    tm += INTERVAL;
+    f.update(t.new_val, tm);
+    EXPECT_NEAR(f.current_estimate(tm), t.estimate_exp, EPS);
   }
 }
